8be6fefec8f0f594e5243c6b7e9b05bb37cb4426
tomkjose <tomjosekunnumpurath@gmail.com>
2023-09-12 23:47:57 +0530

    sign in page added

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,84 @@
 import React from "react";
-
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "../styles/form.css";
+import { Link } from "react-router-dom";
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <form className="form">
+        <div className="header">Sign In</div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          sx={{
+            width: "100%",
+            "& label": {
+              color: "white",
+            },
+            "& .MuiInputBase-input": {
+              color: "white",
+            },
+            "& fieldset": {
+              borderColor: "white",
+            },
+            "&:focus": {
+              "& fieldset": {
+                borderColor: "white",
+              },
+            },
+          }}
+        />
+
+        <TextField
+          required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          sx={{
+            width: "100%",
+            "& label": {
+              color: "white",
+            },
+            "& .MuiInputBase-input": {
+              color: "white",
+            },
+            "& fieldset": {
+              borderColor: "white",
+            },
+            ":hover": {
+              borderColor: "white",
+            },
+          }}
+        />
+        <Button
+          variant="outlined"
+          sx={{
+            color: "#df3a3a",
+            width: "100%",
+            borderColor: "#df3a3a",
+            ":hover": {
+              bgcolor: "#df3a3a",
+              color: "white",
+              borderColor: "#df3a3a",
+            },
+          }}
+        >
+          Login
+        </Button>
+      </form>
+
+      <p className="message">
+        {" "}
+        Create new Account?{" "}
+        <Link to="/signup" style={{ color: "#df3a3a" }}>
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,109 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "../styles/form.css";
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <div>
+        <form className="form">
+          <div className="header">Sign Up</div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            sx={{
+              width: "100%",
+              "& label": {
+                color: "white",
+              },
+              "& .MuiInputBase-input": {
+                color: "white",
+              },
+              "& fieldset": {
+                borderColor: "white",
+              },
+              "&:focus": {
+                "& fieldset": {
+                  borderColor: "white",
+                },
+              },
+            }}
+          />
+
+          <TextField
+            required
+            id="outlined-required"
+            label="Username"
+            sx={{
+              width: "100%",
+              "& label": {
+                color: "white",
+              },
+              "& .MuiInputBase-input": {
+                color: "white",
+              },
+              "& fieldset": {
+                borderColor: "white",
+              },
+              "&:focus": {
+                "& fieldset": {
+                  borderColor: "white",
+                },
+              },
+            }}
+          />
+
+          <TextField
+            required
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            sx={{
+              width: "100%",
+              "& label": {
+                color: "white",
+              },
+              "& .MuiInputBase-input": {
+                color: "white",
+              },
+              "& fieldset": {
+                borderColor: "white",
+              },
+              ":hover": {
+                borderColor: "white",
+              },
+            }}
+          />
+          <Button
+            variant="outlined"
+            sx={{
+              color: "#df3a3a",
+              width: "100%",
+              borderColor: "#df3a3a",
+              ":hover": {
+                bgcolor: "#df3a3a",
+                color: "white",
+                borderColor: "#df3a3a",
+              },
+            }}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+
+        <p className="message">
+          {" "}
+          Already have an account?{" "}
+          <Link to="/signin" style={{ color: "#df3a3a" }}>
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
